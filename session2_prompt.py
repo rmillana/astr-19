1c166acb2c1085595623cb49a0ef337f2f3b5408
rmillana <rmillana@ucsc.edu>
2025-10-15 14:49:32 -0700

    Session 2 Prompt Coding Journal 1.

---
+++ session2_prompt.py
@@ -0,0 +1,17 @@
+float1 = 2.5
+float2 = 8.17
+
+sum = float1 + float2
+print("Sum: ", sum)
+print("Data type of sum: ", type(sum))
+
+int1 = 17
+int2 = 67
+
+minus_int = int1 - int2
+print("Difference: ", minus_int)
+print("Data type of difference: ", type(minus_int))
+
+product = float1 * int1
+print("Product: ", product)
+print("Data type of product: ", type(product))
